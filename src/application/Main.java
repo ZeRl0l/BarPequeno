c84879694ef52be0b75bed29d97c2c7ec8d2f54a
Matheus <matheus-holanda@hotmail.com>
2020-03-08 14:59:07 -0300

    Tela main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,24 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
+		
+		Pane root = FXMLLoader.load(getClass().getResource("FXMLMain.fxml"));
 
+		Scene scene = new Scene(root, 1280, 805);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	
 	}
 	
 	public static void main(String[] args) {
