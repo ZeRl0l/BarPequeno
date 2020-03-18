0dec90b36ddac7aa5547d351b34231d31d82858f
Matheus <matheus-holanda@hotmail.com>
2020-03-18 19:50:38 -0300

    Alterando a quantidade de cadeiras ao informar o valor

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -10,6 +10,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 public class MainController implements Initializable{
 	
@@ -30,7 +32,7 @@ public class MainController implements Initializable{
     
     @FXML
     private Button adicionaCadeira;
-	
+    
     @FXML
     private TextArea txtBar;
     
@@ -39,10 +41,57 @@ public class MainController implements Initializable{
     
     @FXML
     private TextArea txtClienteCasa;
+     
+    @FXML
+    private ImageView imgBar0;
+    
+    @FXML
+    private ImageView imgBar1;
+    
+    @FXML
+    private ImageView imgBar2;
+    
+    @FXML
+    private ImageView imgBar3;
+    
+    @FXML
+    private ImageView imgBar4;
+    
+    @FXML
+    private ImageView imgBar5;
+    
+    @FXML
+    private ImageView imgBar6;
+    
+    @FXML
+    private ImageView imgBar7;
+    
+    @FXML
+    private ImageView imgBar8;
+    
+    @FXML
+    private ImageView imgBar9;
+    
+    @FXML
+    private ImageView imgBar10;
+    
+    
+    
+    private final ImageView imgs_bar[] = new ImageView[11];
     
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+        imgs_bar[0] = imgBar0;
+        imgs_bar[1] = imgBar1;
+        imgs_bar[2] = imgBar2;
+        imgs_bar[3] = imgBar3;
+        imgs_bar[4] = imgBar4;
+        imgs_bar[5] = imgBar5;
+        imgs_bar[6] = imgBar6;
+        imgs_bar[7] = imgBar7;
+        imgs_bar[8] = imgBar8;
+        imgs_bar[9] = imgBar9;
+        imgs_bar[10] = imgBar10;
 		
 	}
 	
@@ -60,7 +109,7 @@ public class MainController implements Initializable{
 		clienteTb.clear();
 		clienteTc.clear();
 		
-		//TODO
+		//TODO não criar cliente caso bar esteja sem cadeiras
 	}
 	
 	@FXML
@@ -68,13 +117,16 @@ public class MainController implements Initializable{
 		try { 
 			int numCadeiras = Integer.parseInt(cadeirasQntd.getText());
 			
-	        if(!(numCadeiras >= 1 && numCadeiras <=  10)){
+	        if(!(numCadeiras >= 1 && numCadeiras <=  10)) {
 	            throw new NumberFormatException();
 	        }
+	        
 	        logBar("Foram adicionadas " + numCadeiras + " cadeiras.");
 	        
-	        //TODO
-	        
+	        imgs_bar[0].setVisible(false);
+            imgs_bar[numCadeiras].setVisible(true);
+            
+            cadeirasQntd.setDisable(true);
             adicionaCadeira.setDisable(true);
 		}
 		catch(NumberFormatException e) {
